23508bd71cade26f0457036c30fe367eea342e8e
VortexEnigma <ggubien@gmail.com>
2024-09-16 16:15:12 +0200

    A more advanced coolblue automation
    "Fixed" the browser selection by manually pressing on it, but the rest seems to function well.

--- Utility/browser.py
+++ Utility/browser.py
@@ -15,27 +15,43 @@ class Browser:
         self.driver.get("https://www.coolblue.be/nl")
 
     def find_element_by_xpath(self, xpath:str):
-        try:
-            element = WebDriverWait(self.driver, self.default_wait_time).until(
-                EC.visibility_of_element_located((By.XPATH, xpath))
-            )
-            return element
-        except Exception as e:
-            print(f"Exception occurred while finding element by XPath: {xpath}")
-            print(e)
-            print(self.driver.page_source)
-
-    def choose_search_engine(self):
-        search_engine_options = self.driver.find_elements(By.XPATH, "//cr-radio-button")
-
-        for option in search_engine_options:
-            if "google" in option.text:
-                option.click()
-                break
-
-    def confirm_search_engine(self):
-        confirm_button_xpath = self.xpath["Search_engine_choice"]["confirm_search_engine"]
-        self.find_element_by_xpath(confirm_button_xpath).click()
+        return WebDriverWait(self.driver,self.default_wait_time).until(EC.visibility_of_element_located((By.XPATH, xpath)))
 
     def allow_cookies(self):
-        self.find_element_by_xpath(self.xpath["Home_page"]["accept_cookies_button"]).click()
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["accept_cookies_button"]).click()
+
+    def select_dropdown(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["language_dropdown_button"]).click()
+
+    def select_language_from_dropdown(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["language_button"]).click()
+
+    def select_category_dropdown(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["dropdown_phones"]).click()
+
+    def select_category_iphone(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["category_apple_iphone_button"]).click()
+
+    def apple_iphone_deals(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["apple_iphone_deals_button"]).click()
+
+    def sorting_products(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["sort_products_dropdown"]).click()
+
+    def sorted_by_highest_rated(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["sorting_products_by_highest_rated"]).click()
+
+    def add_to_favorites(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["add_to_favorites_button"]).click()
+
+    def go_back(self):
+        self.driver.back()
+
+    def iphone_15_plus_128gb_black(self):
+            self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_128GB_Black"]).click()
+
+    def iphone_15_plus_128gb_pink(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_128GB_Pink"]).click()
+
+    def iphone_15_plus_256_green(self):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_256GB_Green"]).click()
--- main.py
+++ main.py
@@ -4,7 +4,33 @@ from time import sleep
 coolblue_tester = Browser()
 
 coolblue_tester.open_url()
-coolblue_tester.choose_search_engine()
-coolblue_tester.confirm_search_engine()
-sleep (5)
+
+# select default browser manually
+sleep (10)
+
 coolblue_tester.allow_cookies()
+coolblue_tester.select_dropdown()
+coolblue_tester.select_language_from_dropdown()
+coolblue_tester.select_category_dropdown()
+coolblue_tester.select_category_iphone()
+coolblue_tester.apple_iphone_deals()
+coolblue_tester.sorting_products()
+coolblue_tester.sorted_by_highest_rated()
+coolblue_tester.iphone_15_plus_128gb_black()
+coolblue_tester.add_to_favorites()
+coolblue_tester.go_back()
+
+sleep(3)
+
+coolblue_tester.iphone_15_plus_128gb_pink()
+coolblue_tester.add_to_favorites()
+coolblue_tester.go_back()
+
+sleep(3)
+
+coolblue_tester.iphone_15_plus_256_green()
+coolblue_tester.add_to_favorites()
+coolblue_tester.go_back()
+
+# Review the Automation in the given time
+sleep(120)
