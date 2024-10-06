d5fe3634186411c2b9200612b7253a2dbbd630ef
VortexEnigma <ggubien@gmail.com>
2024-10-06 15:17:36 +0200

    Solution of Issue #6

--- Utility/browser.py
+++ Utility/browser.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,12 +9,19 @@ import json
 class Browser:
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.default_wait_time = 10
+        self.default_wait_time = 20
         with open ("xpath.json", "r") as file:
             self.xpath = json.load(file)
 
+
     def open_url(self):
         self.driver.get("https://www.coolblue.be/nl")
+
+    def find_child_elements(self, xpath, tag_name):
+        return self.find_element_by_xpath(xpath).find_elements(By.TAG_NAME, tag_name)
+
+    def find_element_by_text(self, text):
+        return WebDriverWait(self.driver,self.default_wait_time).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, text)))
 
     def find_element_by_xpath(self, xpath:str):
         return WebDriverWait(self.driver,self.default_wait_time).until(EC.visibility_of_element_located((By.XPATH, xpath)))
@@ -20,11 +29,12 @@ class Browser:
     def allow_cookies(self):
         self.find_element_by_xpath(self.xpath["Coolblue_Website"]["accept_cookies_button"]).click()
 
-    def select_dropdown(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["language_dropdown_button"]).click()
-
-    def select_language_from_dropdown(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["language_button"]).click()
+    def select_language(self, required_language=None):
+        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["languge_dropdown_button"]).click()
+        language_options = self.find_child_elements(self.xpath["Coolblue_Website"]["language_dropdown_ul"], "li")
+        for language_option in language_options:
+            if language_option.text == required_language:
+                language_option.click()
 
     def select_category_dropdown(self):
         self.find_element_by_xpath(self.xpath["Coolblue_Website"]["dropdown_phones"]).click()
@@ -38,20 +48,27 @@ class Browser:
     def sorting_products(self):
         self.find_element_by_xpath(self.xpath["Coolblue_Website"]["sort_products_dropdown"]).click()
 
-    def sorted_by_highest_rated(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["sorting_products_by_highest_rated"]).click()
+    def sorted_by_most_popular(self):
+        sort_options = self.find_child_elements(self.xpath["Coolblue_Website"]["sort_products_dropdown"], "option")
+        for sort_option in sort_options:
+            if sort_option.text == "Most popular":
+                sort_option.click()
+                break
+
+    def select_devices_by_amount(self, required_amount: int = 2):
+        selected_devices = self.find_child_elements(self.xpath["Coolblue_Website"]["product_container"], "div")
+        wanted_devices = selected_devices[:required_amount]
+        for phone in wanted_devices:
+            print(phone)
+
+
 
     def add_to_favorites(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["add_to_favorites_button"]).click()
+        self.find_element_by_text("Save for later").click()
 
     def go_back(self):
         self.driver.back()
 
-    def iphone_15_plus_128gb_black(self):
-            self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_128GB_Black"]).click()
+    def quit(self):
+        self.driver.quit()
 
-    def iphone_15_plus_128gb_pink(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_128GB_Pink"]).click()
-
-    def iphone_15_plus_256_green(self):
-        self.find_element_by_xpath(self.xpath["Coolblue_Website"]["Apple_iPhone_15_Plus_256GB_Green"]).click()
--- main.py
+++ main.py
@@ -5,32 +5,16 @@ coolblue_tester = Browser()
 
 coolblue_tester.open_url()
 
-# select default browser manually
-sleep (10)
+sleep (3)
 
 coolblue_tester.allow_cookies()
-coolblue_tester.select_dropdown()
-coolblue_tester.select_language_from_dropdown()
+coolblue_tester.select_language()
 coolblue_tester.select_category_dropdown()
 coolblue_tester.select_category_iphone()
 coolblue_tester.apple_iphone_deals()
-coolblue_tester.sorting_products()
-coolblue_tester.sorted_by_highest_rated()
-coolblue_tester.iphone_15_plus_128gb_black()
-coolblue_tester.add_to_favorites()
-coolblue_tester.go_back()
+coolblue_tester.sorted_by_most_popular()
+coolblue_tester.select_devices_by_amount(10)
 
-sleep(3)
+sleep(10)
 
-coolblue_tester.iphone_15_plus_128gb_pink()
-coolblue_tester.add_to_favorites()
-coolblue_tester.go_back()
-
-sleep(3)
-
-coolblue_tester.iphone_15_plus_256_green()
-coolblue_tester.add_to_favorites()
-coolblue_tester.go_back()
-
-# Review the Automation in the given time
-sleep(120)
+coolblue_tester.quit()
